5d5c71e96ba06e9af0c721142ce53020e1db760a
Hasee28 <tohazsha@gmail.com>
2025-12-13 20:42:13 +0530

    KthLargestElement

--- BinaryHeap/KthLargestElement.java
+++ BinaryHeap/KthLargestElement.java
@@ -1,5 +1,42 @@
 package BinaryHeap;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class KthLargestElement {
-    
+    class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if(k<=nums.length){
+            PriorityQueue<Integer> p=new PriorityQueue<>(Collections.reverseOrder());
+            for(int i=0;i<nums.length;i++){
+                p.add(nums[i]);
+            }
+            for(int i=1;i<=k-1;i++)
+                p.poll();
+            return p.peek();
+        }
+        else{
+            PriorityQueue<Integer> p=new PriorityQueue<>();
+            for(int i=0;i<nums.length;i++){
+                p.add(nums[i]);
+            }
+            for(int i=1;i<=k-1;i++)
+                p.poll();
+            return p.peek();
+        }
+      
+    }
+}
+
+// class Solution {
+//     public int findKthLargest(int[] nums, int k) {
+//         PriorityQueue<Integer> p=new PriorityQueue<>(Collections.reverseOrder());
+//         for(int i=0;i<nums.length;i++){
+//             p.add(nums[i]);
+//         }
+//         for(int i=1;i<=k-1;i++)
+//             p.poll();
+//         return p.peek();
+//     }
+// }
 }

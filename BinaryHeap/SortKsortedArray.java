6873bc374b613ba0e9ac0469f3ffb84b3fe8bf60
Hasee28 <tohazsha@gmail.com>
2025-12-16 11:20:26 +0530

    Binary heap problems

--- BinaryHeap/SortKsortedArray.java
+++ BinaryHeap/SortKsortedArray.java
@@ -1,4 +1,18 @@
-package BinaryHeap;
-public class SortKsortedArray {
-    
+//GeeksForgeeks Problem Link:https://www.geeksforgeeks.org/problems/nearly-sorted-1587115620/1
+class Solution {
+    public void nearlySorted(int[] arr, int k) {
+        // code here
+        PriorityQueue<Integer> p=new PriorityQueue<>();
+        for(int i=0;i<=k;i++){
+            p.add(arr[i]);
+        }
+        int j=0;
+        for(int i=k+1;i<arr.length;i++){
+            arr[j++]=p.poll();
+            p.add(arr[i]);
+        }
+        while(!p.isEmpty()){
+            arr[j++]=p.poll();
+        }
+    }
 }
